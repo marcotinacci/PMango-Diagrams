1846c1231ed73288fbb4ce43f8d242cfa9d42d2a
massimo-nocentini <massimo.nocentini@gmail.com>
2010-01-09 17:08:12 +0000

    finish to implement the dependency relation like a set of pair

--- src/PMango/modules/projects/lib/taskdatatree/TaskDataTree.php
+++ src/PMango/modules/projects/lib/taskdatatree/TaskDataTree.php
@@ -11,17 +11,17 @@
  * @copyright Copyright (c) 2009, Kiwi Team
  */
 class TaskDataTree {
-	
+
 	/**
 	 * Variabile di tipo TaskData, è la radice della struttura
 	 * @var TaskData
 	 */
 	private $root;
-	
-	 public function __construct(){
+
+	public function __construct(){
 		$root = new TaskData();
 	}
-	
+
 	/**
 	 * Metodo accessore alla struttura
 	 * @return TaskData
@@ -29,30 +29,30 @@ class TaskDataTree {
 	public function getRoot(){
 		return $this->root;
 	}
-	
+
 	public function setRoot($root){
 		$this->root = $root;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @return vettore di TaskData
 	 */
 	public function deepVisit(){
 		$res = $this->root->deepVisit();
 		return $res;
 	}
-	
+
 	public function wideVisit(){
 		$res = $this->root->wideVisit();
 		return $res;
 	}
-	
+
 	public function getLeaves(){
 		$leaves = $this->root->getLeaves();
 		return $leaves;
 	}
-	
+
 	public function selectTask($task_id){
 		$nodes = $this->wideVisit();
 		for($i=0; $i<sizeOf($nodes); $i++){
@@ -61,7 +61,7 @@ class TaskDataTree {
 			}
 		}
 	}
-	
+
 	public function setAllDependencies(){
 		//prendo tutti i nodi dell'albero
 		$nodes = wideVisit();
@@ -82,6 +82,61 @@ class TaskDataTree {
 			//re-inizializzo current_dep
 			$current_dep = array();
 		}
+	}
+
+	/**
+	 * This method implement a dependency relation between visible tasks
+	 * The relation is implemented in the natural way to define a relation: a set of
+	 * paired object. A pair has this model: (neededTask, dependencyTask)
+	 *
+	 * @return array of pair described above
+	 */
+	public function computeDependencyRelationOnVisibleTasks() {
+		$leaves = $this->getVisibleTree()->getLeaves();
+		$result = array();
+		foreach ($leaves as $leaf) {
+			$deepChildren = $leaf->getInfo()->getCTask()->getDeepChildren();
+			foreach ($deepChildren as $child) {
+				$dependencies = CTask::staticGetDependencies($child);
+				foreach($dependencies as $dependency) {
+					if(in_array($dependency, $deepChildren)) {
+						continue;
+					}
+					else {
+						$neededLeaf = $this->searchNeededLeafThatHaveInDeepChildren($dependency, $leaf);
+						if($neededLeaf != false) {
+							if(!isset($result[$neededLeaf->getInfo()->getTaskID()])) {
+								$result[$neededLeaf->getInfo()->getTaskID()] = array();
+							}
+							$leafId = $leaf->getInfo()->getTaskID();
+							if(!in_array($leafId, $result[$neededLeaf->getInfo()->getTaskID()])) {
+								$result[$neededLeaf->getInfo()->getTaskID()][] = $leafId;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Metodo che ritorna la foglia contenente il task necessario in una relazione di dipendenza
+	 * @param integer $dependency the task identifier of the needed task
+	 * @param TaskData $butLeaf la foglia da saltare per evitare confronti che sicuramente si conosce che 
+	 * 	la foglia non sia presente
+	 * @return TaskData if the leaf is found, else false
+	 */
+	public function searchNeededLeafThatHaveInDeepChildren($dependency, $butLeaf = null) {
+		foreach ($this->getVisibleTree()->getLeaves() as $leaf) {
+			if ($butLeaf != null && $leaf->getInfo()->getTaskID() == $butLeaf->getInfo()->getTaskID()) {
+				continue;
+			}
+
+			if (in_array($dependency, $leaf->getInfo()->getCTask()->getDeepChildren())) {
+				return $leaf;
+			}
+		}
+		return false;
 	}
 	
 	/**
