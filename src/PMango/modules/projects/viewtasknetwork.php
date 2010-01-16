5576c0c71fd0cb1bede5d1b5be3a6e51f7b208c9
massimo-nocentini <massimo.nocentini@gmail.com>
2010-01-16 17:23:22 +0000

    changed the body of the tn page to be sync with the other ones

--- src/PMango/modules/projects/viewtasknetwork.php
+++ src/PMango/modules/projects/viewtasknetwork.php
@@ -4,10 +4,10 @@
 
  PMango Project
 
- Title:      view Gantt.
+ Title:      view Gantt
 
  File:       viewgantt.php
- Location:   pmango\modules\tasks
+ Location:   pmango\modules\projects
  Started:    2005.09.30
  Author:     Lorenzo Ballini
  Type:       PHP
@@ -15,12 +15,12 @@
  This file is part of the PMango project
  Further information at: http://pmango.sourceforge.net
 
- Version history. 
+ Version history.
  - 2006.07.30 Lorenzo
-   Second version, modified to view PMango Gantt.
+   Second version, modified to manage PMango Gantt.
  - 2006.07.30 Lorenzo
    First version, unmodified from dotProject 2.0.1.
-   
+
 ---------------------------------------------------------------------------
 
  PMango - A web application for project planning and control.
@@ -30,7 +30,7 @@
 
  PMango reuses part of the code of dotProject 2.0.1: dotProject code is 
  released under GNU GPL, further information at: http://www.dotproject.net
- Copyright (C) 2003-2005 The dotProject Development Team.
+ Copyright (c) 2003-2005 The dotProject Development Team.
 
  Other libraries used by PMango are redistributed under their own license.
  See ReadMe.txt in the root folder for details. 
@@ -42,7 +42,7 @@
 
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
@@ -52,201 +52,127 @@
 ---------------------------------------------------------------------------
 */
 
-GLOBAL $min_view, $m, $a;
+GLOBAL  $group_id, $min_view, $m, $a;
 
-// re-set the memory limit for gantt chart drawing acc. to the config value of reset_memory_limit
-ini_set('memory_limit', dPgetParam($dPconfig, 'reset_memory_limit', 8*1024*1024));
-
+ini_set('memory_limit', $dPconfig['reset_memory_limit']);
 $min_view = defVal( @$min_view, false);
-
 $project_id = defVal( @$_GET['project_id'], 0);
 
-// sdate and edate passed as unix time stamps
-$sdate = dPgetParam( $_POST, 'sdate', 0 );
-$edate = dPgetParam( $_POST, 'edate', 0 );
-$showLabels = dPgetParam( $_POST, 'showLabels', '0' );
-//if set GantChart includes user labels as captions of every GantBar
-if ($showLabels!='0') {
-    $showLabels='1';
-}
-$showWork = dPgetParam( $_POST, 'showWork', '0' );
-if ($showWork!='0') {
-    $showWork='1';
-}
+require_once dirname(__FILE__)."/lib/useroptionschoice/UserOptionEnumeration.php"; 
+require_once dirname(__FILE__)."/lib/useroptionschoice/UserOptionsChoice.php";
 
-// months to scroll
-$scroll_date = 1;
-
-$display_option = dPgetParam( $_POST, 'display_option', 'this_month' );
-
-// format dates
-$df = $AppUI->getPref('SHDATEFORMAT');
-
-if ($display_option == 'custom') {
-	// custom dates
-	$start_date = intval( $sdate ) ? new CDate( $sdate ) : new CDate();
-	$end_date = intval( $edate ) ? new CDate( $edate ) : new CDate();
-} else {
-	// month
-	$start_date = new CDate();
-	$end_date = new CDate();
-	$end_date->addMonths( $scroll_date );
-}
-
-// setup the title block
-if (!@$min_view) {
-	$titleBlock = new CTitleBlock( 'Gantt Chart', 'applet-48.png',$m, "$m.$a" );
-	//$titleBlock->addCrumb( "?m=tasks", "tasks list" );
-	$titleBlock->addCrumb( "?m=projects&a=view&project_id=$project_id", "View project" );
-	$titleBlock->show();
-}
+//$uoc = new UserOptionsChoice();
+$uoc = UserOptionsChoice::GetInstance();
+$uoc->setFromArray($_POST);
+$_SESSION['uoc'] = serialize($uoc);
 ?>
+
 <script language="javascript">
-var calendarField = '';
 
-function popCalendar( field ){
-	calendarField = field;
-	idate = eval( 'document.editFrm.' + field + '.value' );
-	window.open( 'index.php?m=public&a=calendar&dialog=1&callback=setCalendar&date=' + idate, 'calwin', 'width=250, height=220, scollbars=false' );
+function getPageWidth()
+{
+	//IE
+	if(!window.innerWidth)
+	{
+		return document.body.clientWidth-40;
+	}
+	//w3c
+	return window.innerWidth-40;
 }
 
-/**
- *	@param string Input date in the format YYYYMMDD
- *	@param string Formatted date
- */
-function setCalendar( idate, fdate ) {
-	fld_date = eval( 'document.editFrm.' + calendarField );
-	fld_fdate = eval( 'document.editFrm.show_' + calendarField );
-	fld_date.value = idate;
-	fld_fdate.value = fdate;
+function BuildImage(placeHolder)
+{
+	var divImage = document.getElementById(placeHolder);
+	divImage.innerHTML = "<img src='<?php echo "./modules/projects/lib/chartGenerator/testTN.php?project_id=".$_REQUEST['project_id']."&".UserOptionEnumeration::$FitInWindowWidthUserOption."="; ?>"+getPageWidth()+"'>";
 }
-
-function scrollPrev() {
-	f = document.editFrm;
-<?php
-	$new_start = $start_date;
-	$new_end = $end_date;
-	$new_start->addMonths( -$scroll_date );
-	$new_end->addMonths( -$scroll_date );
-	echo "f.sdate.value='".$new_start->format( FMT_TIMESTAMP_DATE )."';";
-	echo "f.edate.value='".$new_end->format( FMT_TIMESTAMP_DATE )."';";
-?>
-	document.editFrm.display_option.value = 'custom';
-	f.submit()
-}
-
-function scrollNext() {
-	f = document.editFrm;
-<?php
-	$new_start = $start_date;
-	$new_end = $end_date;
-	$new_start->addMonths( $scroll_date+1 );
-	$new_end->addMonths( $scroll_date+1 );
-	echo "f.sdate.value='" . $new_start->format( FMT_TIMESTAMP_DATE ) . "';";
-	echo "f.edate.value='" . $new_end->format( FMT_TIMESTAMP_DATE ) . "';";
-?>
-	document.editFrm.display_option.value = 'custom';
-	f.submit()
-}
-
-function showThisMonth() {
-	document.editFrm.display_option.value = "this_month";
-	document.editFrm.submit();
-}
-
-function showFullProject() {
-	document.editFrm.display_option.value = "all";
-	document.editFrm.submit();
-}
-
 </script>
 
-<table border="0" cellpadding="4" cellspacing="0" align="center">
-
-<form name="editFrm" method="post" action="?<?php echo "m=$m&a=$a&project_id=$project_id";?>">
-<input type="hidden" name="display_option" value="<?php echo $display_option;?>" />
-
+<table width="100%" border="0" cellpadding="4" cellspacing="0">
 <tr>
-	<td align="left" valign="top" width="20">
-<?php 
-	$new_start->addMonths( -$scroll_date );
-	$new_end->addMonths( -$scroll_date );
-	if ($display_option != "all") { ?>
-		<a href="javascript:scrollPrev()">
-			<img src="./images/prev.gif" width="16" height="16" alt="<?php echo $AppUI->_( 'previous' );?>" border="0">
-		</a>
-<?php } ?>
-	</td>
-
-	<td align="right" nowrap="nowrap"><?php echo $AppUI->_( 'From' );?>:</td>
-	<td align="left" nowrap="nowrap">
-		<input type="hidden" name="sdate" value="<?php echo $start_date->format( FMT_TIMESTAMP_DATE );?>" />
-		<input type="text" class="text" name="show_sdate" value="<?php echo $start_date->format( $df );?>" size="12" disabled="disabled" />
-		<a href="javascript:popCalendar('sdate')"><img src="./images/calendar.gif" width="24" height="12" alt="" border="0"></a>
-	</td>
-
-	<td align="right" nowrap="nowrap"><?php echo $AppUI->_( 'To' );?>:</td>
-	<td align="left" nowrap="nowrap">
-		<input type="hidden" name="edate" value="<?php echo $end_date->format( FMT_TIMESTAMP_DATE );?>" />
-		<input type="text" class="text" name="show_edate" value="<?php echo $end_date->format( $df );?>" size="12" disabled="disabled" />
-		<a href="javascript:popCalendar('edate')"><img src="./images/calendar.gif" width="24" height="12" alt="" border="0"></a>
-	<!--<td valign="top">
-		<input type="checkbox" name="showLabels" <?php //echo (($showLabels==1) ? "checked=true" : "");?>><?php //echo $AppUI->_( 'Show captions' );?>
-	</td>
-	<td valign="top">
-		<input type="checkbox" name="showWork" <?php //echo (($showWork==1) ? "checked=true" : "");?>><?php //echo $AppUI->_( 'Show work instead of duration' );?>
-	</td>-->	
-	<td align="left">
-		<input type="button" class="button" value="<?php echo $AppUI->_( 'submit' );?>" onclick='document.editFrm.display_option.value="custom";if (document.editFrm.edate.value < document.editFrm.sdate.value) alert("Start date must before end date"); else submit();'>
-	</td>
-
-	<td align="right" valign="top" width="20">
-<?php if ($display_option != "all") { ?>
-	  <a href="javascript:scrollNext()">
-	  	<img src="./images/next.gif" width="16" height="16" alt="<?php echo $AppUI->_( 'next' );?>" border="0">
-	  </a>
-<?php } ?>
+    <td>
+         <table border="0" cellpadding="4" cellspacing="0" width='100%'>
+			<tr>
+				<form name="editFrm" method="POST" action="?<?php echo "m=$m&a=$a&project_id=$project_id"; ?>">
+                <td valign="top" align="left" nowrap="nowrap">
+                	<b>Show:</b>&nbsp;
+                </td>
+                <td valign="top" align="left" nowrap="nowrap">                          
+                    <input type="checkbox" value='4' name="<?php echo UserOptionEnumeration::$TaskNameUserOption ?>" <?php echo $uoc->showTaskNameUserOption()?"checked":""; ?>> <?php echo "TaskNames"; ?><br>
+                	<input type="checkbox" value='7' name="<?php echo UserOptionEnumeration::$AlertMarkUserOption ?>" <?php echo $uoc->showAlertMarkUserOption()?"checked":""; ?>> <?php echo "AlertMarks"; ?>
+                </td>
+                <td valign="top" align="left" nowrap="nowrap">
+                    <input type="checkbox" value='1' name="<?php echo UserOptionEnumeration::$PlannedDataUserOption ?>" <?php echo $uoc->showPlannedDataUserOption()?"checked":""; ?>> <?php echo "Planned Data"; ?><br>
+                    <input type="checkbox" value='2' name="<?php echo UserOptionEnumeration::$PlannedTimeFrameUserOption ?>" <?php echo $uoc->showPlannedTimeFrameUserOption()?"checked":""; ?>> <?php echo "Planned TimeFrame"; ?>      
+                </td>   
+                <td valign="top" align="left" nowrap="nowrap">      
+                	<input type="checkbox" value='5' name="<?php echo UserOptionEnumeration::$ActualDataUserOption ?>" <?php echo $uoc->showActualDataUserOption()?"checked":""; ?>> <?php echo "Actual Data"; ?><br>
+                	<input type="checkbox" value='6' name="<?php echo UserOptionEnumeration::$ActualTimeFrameUserOption ?>" <?php echo $uoc->showActualTimeFrameUserOption()?"checked":""; ?>> <?php echo "Actual TimeFrame"; ?>
+                </td>
+                <td valign="top" align="left" nowrap="nowrap">
+                	<input type="checkbox" value='8' name="<?php echo UserOptionEnumeration::$ResourcesUserOption ?>" <?php echo $uoc->showResourcesUserOption()?"checked":""; ?>> <?php echo "Resources"; ?>
+                	<input type="hidden" name="<?php echo  UserOptionEnumeration::$TodayDateUserOption;?>" value="<?php echo date("Ymd"); ?>";/>;
+                </td>
+                <td>&nbsp;&nbsp;</td>
+                <td valign="top" align="left" nowrap="nowrap">
+                	<b>Image options:</b>&nbsp;
+                </td>
+                <td valign="top" align="left" nowrap="nowrap">
+                	<?php
+						$v = $uoc->getImageDimensionUserOption();
+					?>
+                	<select name="<?php echo UserOptionEnumeration::$ImageDimensionsUserOption;?>">
+						<option value="<?php echo ImageDimension::$FitInWindowDimUserOption; ?>" <?php echo $v==ImageDimension::$FitInWindowDimUserOption?"selected=\"selected\"":"";?>>Fit window</option>
+						<option value="<?php echo ImageDimension::$CustomDimUserOption; ?>" <?php echo $v==ImageDimension::$CustomDimUserOption?"selected=\"selected\"":"";?>>Custom</option>
+						<option value="<?php echo ImageDimension::$OptimalDimUserOption; ?>" <?php echo $v==ImageDimension::$OptimalDimUserOption?"selected=\"selected\"":"";?>>Optimal</option>
+						<option value="<?php echo ImageDimension::$DefaultDimUserOption; ?>" <?php echo $v==ImageDimension::$DefaultDimUserOption?"selected=\"selected\"":"";?>>Default</option>
+					</select>
+				</td>
+				<td valign="top" align="left" nowrap="nowrap">
+					<?php $wh = $uoc->getCustomDimValues(); ?>
+					width: <input size="4" type="text" name="<?php echo UserOptionEnumeration::$CustomWidthUserOption; ?>" value="<?php echo $wh['width'];?>"/> px <br>
+					height:<input size="4" type="text" name="<?php echo UserOptionEnumeration::$DefaultHeightUserOption; ?>" value="<?php echo $wh['height'];?>"/> px
+					<input type="hidden" name="<?php echo UserOptionEnumeration::$FitInWindowWidthUserOption; ?>" value="0"/>
+					<input type="hidden" name="<?php echo UserOptionEnumeration::$FitInWindowHeightUserOption; ?>" value="0"/>
+                </td>
+                <td width="100%"></td>
+                <td valign="bottom" align="right">
+                	<input type="button" class="button"
+					value="<?php echo $AppUI->_( 'submit' );?>"
+					onclick='submit();'>
+                </td>		
+                </form>  		
+			</tr>
+		</table> 
+				
+				<?php 
+				
+				//uoc debug
+				/*
+				echo UserOptionEnumeration::$PlannedDataUserOption.":".$uoc->showPlannedDataUserOption()."<br>";
+				echo UserOptionEnumeration::$PlannedTimeFrameUserOption .":".$uoc->showPlannedTimeFrameUserOption()."<br>";
+				echo UserOptionEnumeration::$TaskNameUserOption.":".$uoc->showTaskNameUserOption()."<br>";
+				echo UserOptionEnumeration::$ActualDataUserOption.":".$uoc->showActualDataUserOption()."<br>";
+				echo UserOptionEnumeration::$ActualTimeFrameUserOption.":".$uoc->showActualTimeFrameUserOption()."<br>";
+				echo UserOptionEnumeration::$AlertMarkUserOption.":".$uoc->showAlertMarkUserOption()."<br>";
+				echo UserOptionEnumeration::$ResourcesUserOption.":".$uoc->showResourcesUserOption()."<br>";
+				*/
+				 
+				?>
+				
+				<!-- Generated Image -->
+                <table width="100%" cellspacing="0" cellpadding="0" border="1"
+					align="center" class="tbl">
+				<tr>
+					<td align="center">
+						<div id="imagePlaceHolder">
+						</div>
+					</td>
+				</tr>
+				</table>
 	</td>
 </tr>
-
-</form>
-
-<tr>
-	<td align="center" valign="bottom" colspan="7">
-		<?php echo "<a href='javascript:showThisMonth()'>".$AppUI->_('show this month')."</a> : <a href='javascript:showFullProject()'>".$AppUI->_('show full project')."</a><br>"; ?>
-	</td>
-</tr>
-
 </table>
-
-<?php 
-//	require ("../MangoGanttCPM/taskdatatree/Task.php");
-	//echo realpath("./"); // this line would print the path until PMango2.2.0
-	 require ("./modules/projects/lib/taskdatatree/UnitTests.php");
-	 //require ("./modules/projects/lib/chartgenerator/TestGantt.php");
-?>
-
-<table cellspacing="0" cellpadding="0" border="1" align="center">
-<tr>
-	<td>
-<?php 
-if (db_loadResult( "SELECT COUNT(*) FROM tasks WHERE task_project=$project_id" )) {
-	$src =
-	  "?m=tasks&a=gantt&suppressHeaders=1&project_id=$project_id" .
-	  ( $display_option == 'all' ? '' :
-		'&start_date=' . $start_date->format( "%Y-%m-%d" ) . '&finish_date=' . $end_date->format( "%Y-%m-%d" ) ) .
-	  "&width=' + ((navigator.appName=='Netscape'?window.innerWidth:document.body.offsetWidth)*0.95) + '&showLabels=".$showLabels."&showWork=".$showWork;
-
-	echo "<script>document.write('<img src=\"$src\">')</script>";
-} else {
-	echo $AppUI->_( "No tasks to display" );
-}
-?>
-	</td>
-</tr>
-</table>
-<br />
-<?php 
-// reset the php memory limit to the original php.ini value
-ini_restore('memory_limit');
-?>
+<script language="javascript">
+	BuildImage('imagePlaceHolder');
+</script>
+<?php ini_restore('memory_limit');?>
