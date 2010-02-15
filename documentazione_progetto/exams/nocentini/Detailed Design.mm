f2d23ec679a84ee72d08441b0a392529014555b9
massimo-nocentini <massimo.nocentini@gmail.com>
2010-02-15 16:43:55 +0000

    add a exam folder to holds our presentations

---
+++ documentazione_progetto/exams/nocentini/Detailed Design.mm
@@ -0,0 +1,91 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1266081528499" ID="ID_1954882889" MODIFIED="1266083839682" TEXT="Detailed Design">
+<node CREATED="1266081548373" ID="ID_35424422" MODIFIED="1266083309905" POSITION="right" TEXT="Component Oriented Approach" VSHIFT="-65">
+<node CREATED="1266082041866" ID="ID_1559939783" MODIFIED="1266082432746" TEXT="Integration with existing components">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1266082202762" ID="ID_936100367" MODIFIED="1266082421206" TEXT="data sources"/>
+<node CREATED="1266082422251" ID="ID_1386993106" MODIFIED="1266082505078" TEXT="interactive choices"/>
+<node CREATED="1266082505708" ID="ID_1073358137" MODIFIED="1266082521607" TEXT="reports"/>
+</node>
+<node CREATED="1266082234682" ID="ID_575087356" MODIFIED="1266082347874" TEXT="Objects level">
+<node CREATED="1266081617945" HGAP="21" ID="ID_1198151445" MODIFIED="1266082265998" TEXT="Single Object Specification">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1266081678954" ID="ID_1549095405" MODIFIED="1266081719685" TEXT="Operations"/>
+<node CREATED="1266081724362" ID="ID_1987611587" MODIFIED="1266081732014" TEXT="Axioms"/>
+</node>
+<node CREATED="1266081749626" ID="ID_1784575448" MODIFIED="1266082312055" TEXT="Objects&apos; Relationships">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1266081773098" ID="ID_850460955" MODIFIED="1266081776869" TEXT="Client"/>
+<node CREATED="1266081778234" ID="ID_129643944" MODIFIED="1266081784806" TEXT="Inheritance"/>
+</node>
+<node CREATED="1266082094842" HGAP="19" ID="ID_907970436" MODIFIED="1266082321710" TEXT="Object&apos;s Interactions">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+<node CREATED="1266081788538" ID="ID_393128044" MODIFIED="1266082598278" TEXT="Component&apos;s responsibilities specification">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1266081902617" ID="ID_1914972960" MODIFIED="1266081934261" TEXT="Driven by Analisys"/>
+<node CREATED="1266081935338" ID="ID_1805305426" MODIFIED="1266081955253" TEXT="Refined during the process"/>
+</node>
+<node CREATED="1266082574283" ID="ID_1191476385" MODIFIED="1266082732013" TEXT="Components&apos; interactions">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1266082808364" HGAP="31" ID="ID_1925733559" MODIFIED="1266084193866" POSITION="left" TEXT="Analisys Interface" VSHIFT="-83">
+<node CREATED="1266082868667" ID="ID_1159272052" MODIFIED="1266082905391" TEXT="Use case driven">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1266082921069" ID="ID_481779356" MODIFIED="1266082956793" TEXT="methods specification one-to-one relationship"/>
+<node CREATED="1266082961883" ID="ID_1331914602" MODIFIED="1266083000872" TEXT="refinement with sequence diagrams"/>
+</node>
+<node CREATED="1266082909196" ID="ID_1534425152" MODIFIED="1266083261953" TEXT="domain model -&gt; class model">
+<node CREATED="1266083241916" ID="ID_850783966" MODIFIED="1266083260344" TEXT="refinement of domain model concepts">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1266083262334" ID="ID_817149644" MODIFIED="1266083281049" TEXT="poorly detailed">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+</node>
+<node CREATED="1266082820764" ID="ID_329261200" MODIFIED="1266083373086" POSITION="right" TEXT="Coding interface">
+<node CREATED="1266083305068" ID="ID_1004940167" MODIFIED="1266083357272" TEXT="algorithms hints and suggestions">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1266083373086" ID="ID_953295629" MODIFIED="1266083431393" TEXT="underestimated difficulties">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1266083436780" ID="ID_69786471" MODIFIED="1266083445512" TEXT="critical path finder"/>
+<node CREATED="1266083447053" ID="ID_1884276001" MODIFIED="1266083469376" TEXT="retrieving datas from esisting architecture"/>
+<node CREATED="1266083470013" ID="ID_1548952928" MODIFIED="1266083494409" TEXT="retrieving user interactions"/>
+</node>
+<node CREATED="1266083434621" ID="ID_561196867" MODIFIED="1266083529345" TEXT="poorly fine grained specification of modules">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+<node CREATED="1266082832315" ID="ID_834554878" MODIFIED="1266082840423" POSITION="left" TEXT="Testing interface">
+<node CREATED="1266083539117" ID="ID_1331873283" MODIFIED="1266083778274" TEXT="missing robustness digrams">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1266083574141" ID="ID_195751747" MODIFIED="1266083657865" TEXT="use cases checking"/>
+<node CREATED="1266083662669" ID="ID_724220175" MODIFIED="1266083685161" TEXT="discovery of testing items"/>
+<node CREATED="1266083687118" ID="ID_539096819" MODIFIED="1266083771913" TEXT="discovery test&apos;s execution sequences"/>
+<node CREATED="1266083729149" ID="ID_724483016" MODIFIED="1266083744969" TEXT="better specification of the behaviour"/>
+</node>
+</node>
+<node CREATED="1266083814398" ID="ID_369353461" MODIFIED="1266083837049" POSITION="right" TEXT="comparison with class lecture notes"/>
+<node CREATED="1266083841102" ID="ID_506593889" MODIFIED="1266083969092" POSITION="left" TEXT="comparison with other processes">
+<node CREATED="1266083873118" ID="ID_226669287" MODIFIED="1266083955890" TEXT="top down component discovery">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1266083926590" ID="ID_689219124" MODIFIED="1266083958338" TEXT="bottom up component&apos;s interaction specifications">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1266083969950" ID="ID_1151064646" MODIFIED="1266084029138" TEXT="vs functional decomposition">
+<node CREATED="1266084002062" ID="ID_1560745949" MODIFIED="1266084021546" TEXT="fast stage development">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1266084029566" ID="ID_1307769956" MODIFIED="1266084081074" TEXT="reusability of some components">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+</node>
+</map>
